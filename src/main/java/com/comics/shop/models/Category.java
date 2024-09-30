da013d9ca82a8e2241b61c8bbad130bd19f767de
Danil22May <142904608+Danil22May@users.noreply.github.com>
2024-09-30 19:46:06 +0200

    Category model added

--- src/main/java/com/comics/shop/models/Category.java
+++ src/main/java/com/comics/shop/models/Category.java
@@ -4,31 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
-public class Product {
+public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Category category;
-
-    private String description;
-    private double stars;
-    private String image;
-
 }
