99bec4234b08d5efcf3420ad81aa9f24d21b1843
Danil22May <142904608+Danil22May@users.noreply.github.com>
2024-09-30 19:38:15 +0200

    User and Product models done

---
+++ src/main/java/com/comics/shop/models/Product.java
@@ -0,0 +1,28 @@
+package com.comics.shop.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String category;
+    private String description;
+    private double stars;
+    private String image;
+
+    
+}
