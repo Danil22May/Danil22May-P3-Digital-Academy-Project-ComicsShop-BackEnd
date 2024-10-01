e5bb90e7cac7589f4dd75d722edf26ffe88fe274
Danil22May <142904608+Danil22May@users.noreply.github.com>
2024-10-01 15:26:32 +0200

    ProductService done

--- src/main/java/com/comics/shop/services/ProductService.java
+++ src/main/java/com/comics/shop/services/ProductService.java
@@ -1,16 +1,24 @@
 package com.comics.shop.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import com.comics.shop.models.Product;
 import com.comics.shop.repositories.ProductRepository;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
 
-    @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    public Page<Product> getAllProducts(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return productRepository.findAll(pageable);
     }
 }
