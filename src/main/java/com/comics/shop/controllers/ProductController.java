b1e295f5926bb0584d308288d6031acdf13cef4e
Danil22May <142904608+Danil22May@users.noreply.github.com>
2024-10-02 16:59:11 +0200

    hm

--- src/main/java/com/comics/shop/controllers/ProductController.java
+++ src/main/java/com/comics/shop/controllers/ProductController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.comics.shop.models.Category;
 import com.comics.shop.models.Product;
-import com.comics.shop.repositories.ProductRepository;
 import com.comics.shop.services.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +12,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
