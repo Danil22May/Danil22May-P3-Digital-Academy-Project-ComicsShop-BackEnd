bad4961e9a535b031af4eb33762ed4968f234058
Danil22May <142904608+Danil22May@users.noreply.github.com>
2024-10-16 02:28:51 +0200

    small controller changes

--- src/main/java/com/comics/shop/controllers/UserController.java
+++ src/main/java/com/comics/shop/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.comics.shop.services.UserService;
 import com.comics.shop.models.User;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     private UserService userService;
